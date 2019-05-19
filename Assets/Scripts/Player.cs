a2c62d31095feed1175239283a18e9367e0856ff
carbonroast <zergling3@gmail.com>
2019-05-19 01:28:34 -0400

    added assassin kill method

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Player : Humanoid {
 
     public Vector3 input;
+    public bool fire;
+    public List<GameObject> OverlappingNpcs;
 	// Use this for initialization
 
 	
 	// Update is called once per frame
 	void Update () {
         GetInputs();
+        Attack();
 	}
 
     void FixedUpdate()
@@ -23,11 +26,44 @@ public class Player : Humanoid {
         float walkx = Input.GetAxisRaw("Horizontal");
         float walkz = Input.GetAxisRaw("Vertical");
         input = new Vector3(walkx, 0, walkz);
+
+        fire = Input.GetButtonDown("Fire1");
     }
 
     public override void Walk()
     {
 
         this.transform.Translate( input * Time.fixedDeltaTime * speed);
+    }
+
+    public void Attack()
+    {
+        if (fire)
+        {
+            if(OverlappingNpcs.Count > 0)
+            {
+                OverlappingNpcs[0].gameObject.GetComponent<Humanoid>().Death();
+                OverlappingNpcs.RemoveAt(0);
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Unit"))
+        {
+            Debug.Log("enter");
+            OverlappingNpcs.Add(other.gameObject);
+        }
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Unit"))
+        {
+            Debug.Log("exit");
+            OverlappingNpcs.Remove(other.gameObject);
+        }
     }
 }
